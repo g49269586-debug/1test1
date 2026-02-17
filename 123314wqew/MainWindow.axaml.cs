a170223ba5e94d1aed44f542e5f852e75b3d034d
debug123666 <mikhail.rybkin.9090@mail.ru>
2026-02-17 17:34:42 +0300

    отправка

---
+++ 123314wqew/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace _123314wqew
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
